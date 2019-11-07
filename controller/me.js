24b4f962685e0c1840d804e4368eac64455d6d97
nzawirski <nikon96@gmail.com>
2019-11-07 20:30:40 +0100

    edit profile

--- controller/me.js
+++ controller/me.js
@@ -32,4 +32,48 @@ router.get('/', readToken, (req, res) => {
     })
 })
 
+//edit profile
+router.put('/', readToken, (req, res) => {
+    const { username, password } = req.body;
+    jwt.verify(req.token, config.secretKey, (err, authData) => {
+        if (err) {
+            return res.status(403).json({
+                message: err.message
+            })
+        }
+        User.findById(authData.id)
+            .exec((err, user) => {
+                if (err) {
+                    return res.status(500).json({ message: err.message })
+                };
+                if (!user) {
+                    return res.status(404).send("User not found")
+                }
+
+                if (password) {
+                    Password.findOne({ user: authData.id }).exec((err, _password) => {
+                        if (err) {
+                            return res.status(500).json({ message: err.message })
+                        };
+
+                        bcrypt.hash(password, saltRounds, (err, hash) => {
+                            if (err) return console.error(err);
+                            _password.password = hash
+                            _password.save((err) => {
+                                if (err) return console.error(err);
+                            })
+                        })
+                    })
+                }
+
+                user.username = username || user.username
+
+                user.save((err) => {
+                    if (err) return res.status(400).send(err.message);
+                    res.json(user);
+                })
+            })
+    })
+})
+
 module.exports = router
